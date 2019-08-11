cecb4785ae3c647ab8fb19c3621c2f667ab6b0f6
joe-alfaro <josephlalfaro@gmail.com>
2019-08-10 19:33:39 -0700

    add production config to knex.js

--- knexfile.js
+++ knexfile.js
@@ -17,4 +17,20 @@ module.exports = {
       },
     },
   },
+  production: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      afterCreate: (connection, done) => {
+        connection.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  },
 };
