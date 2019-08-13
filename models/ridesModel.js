174a64149e0e95a7a76b72ac4a8dc70b3ee31eb5
joe-alfaro <josephlalfaro@gmail.com>
2019-08-13 08:53:41 -0700

    change fastpass times output when not available

--- models/ridesModel.js
+++ models/ridesModel.js
@@ -12,10 +12,6 @@ const updateWaitTimes = async () => {
   rideTimes.forEach(async ride => {
     const { id, status, waitTime } = ride;
     let { fastPassStartTime, fastPassEndTime } = ride.meta;
-    if(!fastPassStartTime){
-      fastPassStartTime = '23:59:59';
-      fastPassEndTime = '23:59:59';
-    };
     await db('rides')
       .where({id})
       .update({waitTime, status, fastPassStartTime, fastPassEndTime})
